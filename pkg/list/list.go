f7e5fe8924dba49afde1c31a6ede679ac78ebe89
pspiagicw <pspiagicw@gmail.com>
2023-12-13 07:46:07 +0000

    feat: added --simple listing

--- pkg/list/list.go
+++ pkg/list/list.go
@@ -1,13 +1,38 @@
 package list
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/pspiagicw/goreland"
 	"github.com/pspiagicw/gox/pkg/database"
+	"github.com/pspiagicw/gox/pkg/help"
 )
 
+type options struct {
+	simple bool
+}
+
+func parseListArgs(args []string) ([]string, *options) {
+	flag := flag.NewFlagSet("gox list", flag.ExitOnError)
+	options := new(options)
+	flag.Usage = help.HelpList
+	flag.BoolVar(&options.simple, "simple", false, "Print simple listing")
+	flag.Parse(args)
+	return flag.Args(), options
+}
+
 func ListPackage(args []string) {
+	args, options := parseListArgs(args)
 	gdb := database.ParseDatabase()
 
+	if !options.simple {
+		listDefault(gdb)
+	} else {
+		listSimple(gdb)
+	}
+}
+func listDefault(gdb *database.GoxDatabase) {
 	headers := []string{"Package Name", "Package Path", "Package URL"}
 	rows := [][]string{}
 
@@ -20,5 +45,16 @@ func ListPackage(args []string) {
 
 	} else {
 		goreland.LogTable(headers, rows)
+	}
+
+}
+func listSimple(gdb *database.GoxDatabase) {
+	fmt.Println("INSTALLED PACKAGES:\n")
+	names := []string{}
+	for name, _ := range gdb.Packages {
+		names = append(names, name)
+	}
+	for _, name := range names {
+		fmt.Printf("- [%s]\n", name)
 	}
 }
